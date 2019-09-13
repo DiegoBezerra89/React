e6cb87f12eba33d1806c5e921fac3c33e0eaa46a
Diego Bezerra <fatequino@gmail.com>
2019-09-13 17:55:40 -0300

    Inicial commit contador de voltas

--- 01-contador-voltas/src/App.js
+++ 01-contador-voltas/src/App.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from 'react'
+
+const MostraVoltas = () => { //arrow-function
+  return (
+    <p>10<br/>
+      voltas
+    </p>
+  )  
+}
+
+const ButtonPlus = () => {
+  return (
+    <button>+</button>
+  )
+}
+
+const ButtonSubtract = () => {
+  return (
+    <button>-</button>
+  )
+}
+
+const TempoMedio = () => {
+  return (
+    <p>
+      1:30<br/>
+      Tempo médio por volta
+    </p>
+  )
+}
+
+const ButtonInit = () => {
+  return (
+    <button>Iniciar</button>
+  )
+}
+
+const ButtonReinit = () => {
+  return (
+    <button>Reiniciar</button>
+  )
+}
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className='App'> 
+      <MostraVoltas />     
+      <ButtonPlus /> 
+      <ButtonSubtract />
+      <TempoMedio />
+      <ButtonInit />
+      <ButtonReinit />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
