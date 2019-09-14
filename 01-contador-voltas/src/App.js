1df2148bcc7058ae6665f2a3e99d7a4529f73286
Diego Bezerra <fatequino@gmail.com>
2019-09-14 11:40:39 -0300

    Contador de voltas setado, e counter setado, no meio da explicação sobre useEffect

--- 01-contador-voltas/src/App.js
+++ 01-contador-voltas/src/App.js
@@ -1,56 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-const MostraVoltas = () => { //arrow-function
+//arrow-function || utilizar letras maiúsculas para determinar ao React o que são componentes
+const MostraVoltas = (props) => { //props neste caso, recebe todas as propriedades passadas no JSX, neste caso voltas='15' 
   return (
-    <p>10<br/>
+    <p>{props.voltas}<br/>
       voltas
     </p>
   )  
 }
 
-const ButtonPlus = () => {
-  return (
-    <button>+</button>
-  )
-}
-
-const ButtonSubtract = () => {
-  return (
-    <button>-</button>
-  )
-}
-
-const TempoMedio = () => {
+const TempoMedio = (props) => {
   return (
     <p>
-      1:30<br/>
+      {props.tempo}<br/>
       Tempo médio por volta
     </p>
   )
 }
 
-const ButtonInit = () => {
-  return (
-    <button>Iniciar</button>
-  )
-}
-
-const ButtonReinit = () => {
-  return (
-    <button>Reiniciar</button>
-  )
-}
+//Componente reaproveitável
+const Button = (props) => <button onClick={props.onClick}> {props.text} </button> //não precisa de {} se a fucção tiver apenas uma linha
 
 
 function App() {
+  const [numVoltas, setNumVoltas] = useState(10)
+  const [running, setRunning] = useState(false) //se o programa está rodando ou não, toda vez q o running for setado, o useEffect vai responder criando o intervalo de 1000ms
+  const[tempo, setTempo] =  useState(0)
+
+  useEffect(() => { //possibilita uma chamada de função num determinado tempo
+    let timer = null
+      if(running){
+        timer = setInterval(() => {
+          setTempo(old => old + 1)
+      }, 1000 )
+    }
+    return () => {
+      if(timer) {
+        clearInterval(timer)
+      }
+    }
+  }, [running]) //os colchetes vazios determinam que o useEffect não tem dependências, ele só é chamado uma única vez, caso contrário ele seria chamado todas as vezes que o useState fosse ativado
+  //running é dependencia do useEffect, sempre que for mudado de estado false->true true->false ele executa o useEffect
+
+  const toggleRunning = () => {
+    setRunning(!running) //se for falso, vira verdadeiro e se for verdadeiro vira falso 
+  }
+
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1)
+  }
+  const decrement = () => {
+    setNumVoltas(numVoltas - 1)
+  }
+
+  
+
   return (
     <div className='App'> 
-      <MostraVoltas />     
-      <ButtonPlus /> 
-      <ButtonSubtract />
-      <TempoMedio />
-      <ButtonInit />
-      <ButtonReinit />
+      <MostraVoltas voltas={numVoltas} /> {/* possível passar valores ao componente via props, voltas='15'*/}     
+      <Button text='+' onClick={increment} /> 
+      <Button text='-' onClick={decrement} />
+      <TempoMedio tempo={tempo} />
+      <Button text='Iniciar' onClick={toggleRunning} />
+      <Button text='Reiniciar' />
     </div>
   )
 }
